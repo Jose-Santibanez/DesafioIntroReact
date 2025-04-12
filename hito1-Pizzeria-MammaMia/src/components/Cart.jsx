500eb8ae4abbec09fd4368b785fc9e20e202f0e4
Jose-Santibanez <j.patricio.rqm@outlook.es>
2025-04-12 15:02:54 -0400

    Subiendio cambios en agregar,eliminar y total a pagar

--- hito1-Pizzeria-MammaMia/src/components/Cart.jsx
+++ hito1-Pizzeria-MammaMia/src/components/Cart.jsx
@@ -3,27 +3,11 @@ import {  Button} from "react-bootstrap"
 import { CartContext } from "../context/CartContext"
 
 export const Cart = ()=> {
-       
-    const {cart,setCart,total, handleItems, eliminaItem } = useContext(CartContext)
-    
-   /*  
+    const {cart,total, handleItems, eliminaItem } = useContext(CartContext)
 
-    const yaExiste = id => prueba.find(elem => elem.id == id) != undefined;
     
-    const validaDuplicados = (id)=>{ 
-       let duplicado = prueba.find(e=>e.id == id) !== undefined
-       return duplicado
-    }
     
-    validaDuplicados(1) */
-   
-    console.log(cart)
-    const prueba = [{id: 0 , nombre:'nombre'},{id: 0 , nombre:'nombre'},{id: 1, nombre: 'nombre'}]
-    const existe = (item) => {
-        const duplicados = prueba.filter(e=> e.id === item ) 
-        console.log(duplicados.length)
-    }
-    existe(0)
+
     
     return(
             <div className="contenedor-cart">
@@ -32,10 +16,10 @@ export const Cart = ()=> {
                     </h3>
                     <div>
                     {cart.map(e=>(
-                        <ul className="cart-list"><img src={e.img} alt=""/>
-                            
+                        <ul className="cart-list"> 
+                            <img src={e.img} alt=""/>  
                             <li>{e.name}</li>
-                            <li>{e.price}</li>
+                            <li>{e.price}</li> 
                             <button className="btn-red" onClick={()=>eliminaItem(e)}>-</button>
                             <button onClick={()=>handleItems(e)}>+</button>
                         </ul>
--- hito1-Pizzeria-MammaMia/src/context/CartContext.jsx
+++ hito1-Pizzeria-MammaMia/src/context/CartContext.jsx
@@ -4,34 +4,46 @@ export const CartContext = createContext() // Creamos el contexto llamado CartCo
 
 
 export const CartProvider = ({children})=> {
-     
+    // Creamos los estados que vamos a manejar
     const [pizza, setPizza] = useState([]);
     const [total, setTotal] = useState(0)
     const [cart, setCart] = useState([])    
     
     // Agregar items al carrito
      const handleItems = (itemPizza)=>{ 
-          
-        setCart([...cart,itemPizza]) 
-        const precio = itemPizza.price;
-        calculaTotal(precio,1)
-        }   
+            const index = cart.findIndex((item) => item.id === itemPizza.id); // Buscamos el elemento  existe? nos devuelve el indice : -1 
+            if(index !== -1){ // si es distinto a -1 (es decir encontro el indice)
+                const newCart = [...cart]  // agregamos un nuevo arreglo usando spread operator y lo guardamos en newCart
+                newCart[index].cantidad += 1;  // buscamos dentro del array con el indice encontrado accedemos a la propiedad cantidad y le sumamos 1
+                setCart(newCart);    // agregamos el nuevo carrito alterado en la propiedad cantidad de ese producto en esa posición(newCart[index]) al estado Cart 
+            }  
+            else{
+                setCart([...cart,{...itemPizza,cantidad : 1}]); // de lo contrario es -1, no encontro nada agrega un nuevo array del carrito agregando el nuevo producto y se crea la nueva propiedad en 1  (cantidad : 1)
+            }
+        } 
+        
+        
     // Elimina item del carrito
     const eliminaItem = (itemPizza)=>{
-        console.log(itemPizza)
-        const filtrar = cart.filter(e=> e.id !== itemPizza.id) // filtra los todos los elementos distintos al valor pasado por parametro
-        setCart(filtrar)
-       calculaTotal(itemPizza.price,0)
+        const existe = cart.find(e => e.id == itemPizza.id) // buscamos el primer elemento que encuentra con el id del objeto pasado por parametro,nos devuelve el objeto encontrado
+        if(existe.cantidad > 1){ // si encuentra el valor y cantidad es mayor a 1 
+            const newCart = cart.map( e => e.id === itemPizza.id 
+            ? { ...e, cantidad: e.cantidad - 1 } : e)
+            setCart(newCart)
+        }else{
+            const newCart = cart.filter( e=> e.id !== itemPizza.id)
+            setCart(newCart)
+        }
     }
-    // calculo del total a pagar
-    const calculaTotal = (precio,operación)=> {
-        if(operación === 1)setTotal(prev => prev + precio)
-        else if (operación === 0)setTotal(prev => prev - precio)
+    console.log(cart)
+
+    const calcularTotal = () => {
+        const totalCalculado = cart.reduce((acum,item)=>{
+            return acum + item.price * item.cantidad;
+        },0)
+        setTotal(totalCalculado)
     }
-    // encuentra duplicados
-    const encuentraDuplicados = (item)=> {
-        const duplicados = cart.filtere(e=> e.id === item.id ) 
-    }
+ 
     
     const api = "http://localhost:5000/api/pizzas/"
     
@@ -52,10 +64,11 @@ export const CartProvider = ({children})=> {
     };
     useEffect(()=>{
         getData()
-    },[]);
+        calcularTotal()
+    },[cart]);
 
     return(
-        <CartContext.Provider value={{pizza, setPizza,handleItems,total,cart, eliminaItem,encuentraDuplicados}}>
+        <CartContext.Provider value={{pizza, setPizza,handleItems,total,cart, eliminaItem}}>
                 {children}
         </CartContext.Provider>
     )
